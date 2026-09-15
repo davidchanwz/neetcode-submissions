70dd2032a4fb064860d6eff57ef2e36f78faa698
David Chan <davidchanwz@gmail.com>
2026-09-15 20:47:42 +0800

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        def helper(subarray):
+            rob1, rob2 = 0, 0
+            for num in subarray:
+                tmp = rob1
+                rob1 = max(num + rob2, rob1)
+                rob2 = tmp
+            return max(rob1, rob2)
+        return max(helper(nums[1:]), helper(nums[:-1]))
